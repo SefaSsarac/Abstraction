641e4b3af9a8f917c80589f813beb74fcaabecea
Sefa Saraç <srcsfysf@gmail.com>
2024-08-09 15:34:50 +0300

    Add files via upload

---
+++ Abstraction/Abstraction/Calisan.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    // Abstract Base Class
+    public abstract class Calisan
+    {
+        // Ortak Özellikler
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        // Constructor
+        public Calisan(string ad, string soyad, string departman)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Departman = departman;
+        }
+
+        // Abstract Metot
+        public abstract void Gorev();
+    }
+
+    // YazilimGelistirici sınıfı
+    public class YazilimGelistirici : Calisan
+    {
+        public YazilimGelistirici(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        // Gorev metodunu override ediyoruz
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}, {Departman} departmanında Yazılım Geliştirici olarak çalışıyorum.");
+        }
+    }
+
+    // ProjeYoneticisi sınıfı
+    public class ProjeYoneticisi : Calisan
+    {
+        public ProjeYoneticisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        // Gorev metodunu override ediyoruz
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}, {Departman} departmanında Proje Yöneticisi olarak çalışıyorum.");
+        }
+    }
+
+    // SatisTemsilcisi sınıfı
+    public class SatisTemsilcisi : Calisan
+    {
+        public SatisTemsilcisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        // Gorev metodunu override ediyoruz
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}, {Departman} departmanında Satış Temsilcisi olarak çalışıyorum.");
+        }
+    }
+}
---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,19 @@
+﻿using Abstraction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Yazılım Geliştirici Nesnesi Oluşturma
+        YazilimGelistirici yazilimci = new YazilimGelistirici("Ali", "Veli", "Yazılım");
+        yazilimci.Gorev();
+
+        // Proje Yöneticisi Nesnesi Oluşturma
+        ProjeYoneticisi projeYoneticisi = new ProjeYoneticisi("Hasan", "Çıldırmış", "Proje Yönetimi");
+        projeYoneticisi.Gorev();
+
+        // Satış Temsilcisi Nesnesi Oluşturma
+        SatisTemsilcisi satisTemsilcisi = new SatisTemsilcisi("Ayşe", "Yılmaz", "Satış");
+        satisTemsilcisi.Gorev();
+    }
+}
